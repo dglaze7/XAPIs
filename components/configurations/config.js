f65d64285492ea9dd5eec2044d97601911d07223
dglaze7 <rama_doni17@ymail.com>
2018-07-24 11:23:44 +0700

    add new project

---
+++ components/configurations/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    port : 8000,
+    //dbconn : 'mongodb://localhost:27017/',
+    dbconn : 'mongodb://admin:admin123@ds245971.mlab.com:45971/bebas',
+    dbname : 'bebas'
+}
